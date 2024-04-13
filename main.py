21bc3e4fe6b4dcbc71d7f90d02813c570eb8d7c6
deepanshukla <deepanshukla@gmail.com>
2024-04-13 11:50:43 +0530

    kuch to updated h

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
@@ -77,15 +77,34 @@ def registerform(request: Request, username: Annotated[str, Form()], email: Anno
 @app.get("/logout", response_class=HTMLResponse)
 def logout(request: Request):
     request.session.pop('username')
-    request.session.pop('data1')
-    request.session.pop('labels1')
-    request.session.pop('data2')
-    request.session.pop('data3')
-    request.session.pop('labels3')
-    request.session.pop('data4')
-    request.session.pop('labels4')
     return RedirectResponse(url="/")
 
+
+@app.post("/resetpassword", response_class=HTMLResponse)
+async def reset_password(request: Request, email: str = Form(...),
+                         password: str = Form(...),
+                         confirm_password: str = Form(...)):
+    print(email, password, confirm_password)
+    cur.execute("select username from users where email = :email", {'email': email})
+    result = cur.fetchone()
+    if not result:
+        message = "Email not found"
+        return templates.TemplateResponse("forgotpass.html", {"request": request, "message": message})
+    else:
+        username = result[0]
+        if password != confirm_password:
+            message = "Passwords do not match"
+            return templates.TemplateResponse("forgotpass.html", {"request": request, "message": message})
+        else:
+            cur.execute("update users set password = :password where email = :email",
+                        {'password': password, 'email': email})
+            conn.commit()
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+@app.get("/forgotpassword", response_class=HTMLResponse)
+def forgot_password(request: Request):
+    return templates.TemplateResponse("forgotpass.html", {"request": request})
 
 
 def total_balace(username):
@@ -101,38 +120,40 @@ def total_balace(username):
 def dashboard(request: Request, username: str):
     result = total_balace(username)
     # cur.callfunc('calculate_total_spending', float, [username, datetime.date.today()])
-    dailymoneyspent = dailyspending(username, datetime.date.today())
+    dailymoneyspent = dailyspending(username, datetime.today().date())
     cur.execute(
         "SELECT * FROM Transactions WHERE Username = :username ORDER BY TransactionDate DESC FETCH FIRST 4 ROWS ONLY",
         {'username': username})
     transactions = cur.fetchall()
     data, labels = show_chart(username)
-    cur.execute("""select to_char(ENDDATE,'MONTH'),INCOMETOTAL,EXPENSETOTAL
-                    from REPORTS where USERNAME = :username""", {'username': username})
+    cur.execute("""SELECT TRUNC(TransactionDate) AS TransactionDay,
+                SUM(Amount) AS TotalExpense FROM Transactions
+            WHERE CategoryID IN (3, 4, 5, 6, 7, 8) 
+                AND TransactionDate >= TRUNC(SYSDATE) - 7
+                AND USERNAME = :username
+            GROUP BY
+                TRUNC(TransactionDate) 
+            ORDER BY
+                TRUNC(TransactionDate) DESC""", {'username': username})
     result_set = cur.fetchall()
-    month = []
-    income = []
-    expense = []
-    if result_set:
-        for i in result_set:
-            month.append(i[0])
-            income.append(i[1])
-            expense.append(i[2])
-    else:
-        month = ["No data"]
-        income = [0]
-        expense = [0]
+    days = []
+    expenses = []
+    for i in result_set:
+        days.append(i[0].strftime("%A"))
+        expenses.append(i[1])
+    days.reverse()
+    expenses.reverse()
+    print(days, expenses)
     context = {"request": request, "result": username, "balance": result, "dailyspent": dailymoneyspent,
-               "transactions": transactions, "data": data, "labels": labels, "month": month, "income": income,
-               "expense": expense}
-    return templates.TemplateResponse("index.html", context)
+               "transactions": transactions, "data": data, "labels": labels, "days": days, "expenses": expenses}
+    return templates.TemplateResponse("index2.html", context)
 
 
 def dailyspending(username, date):
     cur.callfunc('calculate_total_spending', float, [username, date])
     dailyspent = cur.var(cx_Oracle.NUMBER)
     cur.execute('BEGIN :result := calculate_total_spending(:username, :today); END;',
-                result=dailyspent, username=username, today=datetime.date.today())
+                result=dailyspent, username=username, today=datetime.today().today())
     dailyspent = dailyspent.getvalue()
     return dailyspent
 
@@ -146,35 +167,6 @@ def get_category_id(category_name):
         return None
 
 
-@app.post("/add_fund", response_class=HTMLResponse)
-def add_funds(request: Request, amount: Annotated[float, Form()], category: Annotated[str, Form()]):
-    amount = float(amount)
-    category_id = get_category_id(category)
-    username = request.session.get('username')
-    transaction_type = category
-
-    cur.execute("INSERT INTO Transactions (TransactionID, Username, CategoryID, Amount, Description, TransactionDate) "
-                "VALUES (transaction_id.nextval, :username, :categoryid, :amount, :Description, SYSDATE)",
-                {'username': username, 'categoryid': category_id, 'amount': amount, 'Description': transaction_type})
-    conn.commit()
-    url = f"/dashboard/{username}"
-    return RedirectResponse(url=url)
-
-
-@app.post("/add_expense", response_class=HTMLResponse)
-def add_expense(request: Request, amount: Annotated[float, Form()], category: Annotated[str, Form()],
-                description: Annotated[str, Form()]):
-    amount = float(amount)
-    category_id = get_category_id(category)
-    username = request.session.get('username')
-    cur.execute("INSERT INTO Transactions (TransactionID, Username, CategoryID, Amount, Description, TransactionDate) "
-                "VALUES (transaction_id.nextval, :username, :categoryid, :amount, :Description, SYSDATE)",
-                {'username': username, 'categoryid': category_id, 'amount': amount, 'Description': description})
-    conn.commit()
-    url = f"/dashboard/{username}"
-    return RedirectResponse(url=url)
-
-
 def show_chart(username):
     result_set_cursor = cur.var(cx_Oracle.CURSOR)
     cur.execute("BEGIN :result := get_expense_spending(:username); END;",
@@ -183,11 +175,10 @@ def show_chart(username):
 
     data = []
     labels = []
-
     for i in result_set:
         labels.append(i[0])
         data.append(i[1])
-
+    # print(data, labels)
     return data, labels
 
 
@@ -197,127 +188,188 @@ def transactions(request: Request, username: str):
     cur.execute("SELECT * FROM Transactions WHERE Username = :username ORDER BY TransactionDate DESC",
                 {'username': username})
     transactions1 = cur.fetchall()
-    context = {"request": request, "transactions": transactions1, "result": username}
-    return templates.TemplateResponse("t_history.html", context)
+    tot_bal = total_balace(username)
+    category_names = {
+        1: "Salary",
+        2: "Deposits",
+        3: "Food",
+        4: "Transportation",
+        5: "Bills",
+        6: "Toiletries",
+        7: "Gifts",
+        8: "Others"
+    }
+    dailyspend = dailyspending(username, datetime.today().date())
+    context = {"request": request, "transactions": transactions1, "result": username, "balance": tot_bal,
+               "dailyspent": dailyspend, "category_names": category_names}
+    return templates.TemplateResponse("transactions.html", context)
+
+
+@app.get("/dashboard/{username}", response_class=HTMLResponse)
+def dashboard(request: Request, username: str):
+    result = total_balace(username)
+    # cur.callfunc('calculate_total_spending', float, [username, datetime.date.today()])
+    dailymoneyspent = dailyspending(username, datetime.today().date())
+    cur.execute(
+        "SELECT * FROM Transactions WHERE Username = :username ORDER BY TransactionDate DESC FETCH FIRST 4 ROWS ONLY",
+        {'username': username})
+    transactions = cur.fetchall()
+    data, labels = show_chart(username)
+    cur.execute("""SELECT TRUNC(TransactionDate) AS TransactionDay,
+                SUM(Amount) AS TotalExpense FROM Transactions
+            WHERE CategoryID IN (3, 4, 5, 6, 7, 8) 
+                AND TransactionDate >= TRUNC(SYSDATE) - 7
+                AND USERNAME = :username
+            GROUP BY
+                TRUNC(TransactionDate) 
+            ORDER BY
+                TRUNC(TransactionDate) DESC""", {'username': username})
+    result_set = cur.fetchall()
+    days = []
+    expenses = []
+    for i in result_set:
+        days.append(i[0].strftime("%A"))
+        expenses.append(i[1])
+    days.reverse()
+    expenses.reverse()
+    print(days, expenses)
+    context = {"request": request, "result": username, "balance": result, "dailyspent": dailymoneyspent,
+               "transactions": transactions, "data": data, "labels": labels, "days": days, "expenses": expenses}
+    return templates.TemplateResponse("index2.html", context)
 
 
 # analytics
-@app.get("/analytics/{username}", response_class=HTMLResponse)
-def analytics(request: Request, username: str):
-    month = datetime.date.today().month
-    result_set_cursor = cur.var(cx_Oracle.CURSOR)
-    cur.execute("BEGIN :result := get_expense_spending_per_month(:username,:month); END;",
-                result=result_set_cursor, username=username, month=month)
 
-    result_set = result_set_cursor.getvalue()
+
+@app.post("/transactions/{username}", response_class=HTMLResponse)
+def transactions(request: Request, username: str):
+    cur.execute("SELECT * FROM Transactions WHERE Username = :username ORDER BY TransactionDate DESC",
+                {'username': username})
+    transactions1 = cur.fetchall()
+    tot_bal = total_balace(username)
+    dailyspend = dailyspending(username, datetime.today().date())
+    category_names = {
+        1: "Salary",
+        2: "Deposits",
+        3: "Food",
+        4: "Transportation",
+        5: "Bills",
+        6: "Toiletries",
+        7: "Gifts",
+        8: "Others"
+    }
+    context = {"request": request, "transactions": transactions1, "result": username, "balance": tot_bal,
+               "dailyspent": dailyspend, "category_names": category_names}
+    return templates.TemplateResponse("transactions.html", context)
+
+
+@app.post("/submit-transaction", response_class=HTMLResponse)
+async def submit_transaction(request: Request, transactionName: str = Form(...),
+                             transactionAmount: float = Form(...),
+                             transactionCategory: str = Form(...)):
+    print(transactionName, transactionAmount, transactionCategory)
+    # Process the form data as needed, such as saving to a database
+    category_id = get_category_id(transactionCategory)
+    username = request.session.get('username')
+    cur.execute(
+        "INSERT INTO Transactions (TransactionID, Username, CategoryID, Amount, Description, TransactionDate) "
+        "VALUES (transaction_id.nextval, :username, :categoryid, :amount, :Description, SYSDATE)",
+        {'username': username, 'categoryid': category_id, 'amount': transactionAmount, 'Description': transactionName})
+    conn.commit()
+    # Redirect to a success page or another URL
+    return RedirectResponse(url=f"/dashboard/{username}")
+
+
+@app.post("/updatetransaction", response_class=HTMLResponse)
+async def update(request: Request, transactionId: int = Form(...), transactionName: str = Form(...),
+                 transactionAmount: float = Form(...), transactionCategory: str = Form(...)):
+    print(transactionId, transactionName, transactionAmount, transactionCategory)
+    username = request.session.get('username')
+    category_id = get_category_id(transactionCategory)
+    cur.execute(
+        "Update Transactions set Description = :transactionName, Amount = :transactionAmount, CategoryID = :transactionCategory where TransactionID = :transactionId",
+        {'transactionName': transactionName, 'transactionAmount': transactionAmount, 'transactionCategory': category_id,
+         'transactionId': transactionId})
+    conn.commit()
+    return RedirectResponse(url=f'/transactions/{username}')
+
+
+@app.post("/deletetransaction", response_class=HTMLResponse)
+async def delete(request: Request, transactionId: int = Form(...)):
+    username = request.session.get('username')
+    cur.execute("Delete from Transactions where TransactionID = :transactionId", {'transactionId': transactionId})
+    conn.commit()
+    return RedirectResponse(url=f'/transactions/{username}')
+
+
+@app.get("/statistics/{username}", response_class=HTMLResponse)
+def statistics(request: Request, username: str):
+    cur.execute("""SELECT CategoryId, SUM(Amount) AS TotalExpense
+                    FROM Transactions
+                    WHERE CategoryId NOT IN (1,2) AND Username = :username
+                    GROUP BY CategoryId""",
+                {'username': username})
+    result_set = cur.fetchall()
     data = []
     labels = []
-
+    category_names = {
+        3: "Food",
+        4: "Transportation",
+        5: "Bills",
+        6: "Toiletries",
+        7: "Gifts",
+        8: "Others"
+    }
     for i in result_set:
-        labels.append(i[0])
+        labels.append(category_names[i[0]])
         data.append(i[1])
-    request.session['data1'] = data
-    request.session['labels1'] = labels
-    data2 = piechart2(username, month-1)
-    request.session['data2'] = data2
-    cur.execute("""SELECT TO_CHAR(ENDDATE, 'Month') AS MONTH_NAME, NETSAVINGS
-                    FROM REPORTS
-                    WHERE USERNAME = :username
-                    order by extract(month from ENDDATE)
+
+    tot_sum = sum(data)
+    data_per = [round((i / tot_sum) * 100, 2) for i in data]
+    print(data, labels)
+    cur.execute("""SELECT TO_CHAR(TransactionDate, 'YYYY-MM') AS Month, SUM(Amount) AS TotalExpense
+                    FROM Transactions
+                    WHERE CategoryId NOT IN (1, 2) -- Assuming CategoryId 1 and 2 are for funds
+                    AND EXTRACT(YEAR FROM TransactionDate) = EXTRACT(YEAR FROM SYSDATE) 
+                    AND Username = :username
+                    GROUP BY TO_CHAR(TransactionDate, 'YYYY-MM')
+                    ORDER BY TO_CHAR(TransactionDate, 'YYYY-MM')
                     """, {'username': username})
     result_set = cur.fetchall()
-    data3 = []
-    labels3 = []
+    data1 = []
+    labels1 = []
     for i in result_set:
-        labels3.append(i[0])
-        data3.append(i[1])
-    request.session['data3'] = data3
-    request.session['labels3'] = labels3
-    cur.execute("""SELECT TO_CHAR(ENDDATE, 'Month') AS MONTH_NAME, expensetotal
-                        FROM REPORTS
-                        WHERE USERNAME = :username
-                        order by extract(month from ENDDATE)
+        labels1.append(datetime.strptime(i[0], '%Y-%m').strftime('%B'))
+        data1.append(i[1])
+    cur.execute("""SELECT TO_CHAR(TransactionDate, 'YYYY-MM') AS Month, SUM(Amount) AS TotalExpense
+                        FROM Transactions
+                        WHERE CategoryId IN (1, 2) -- Assuming CategoryId 1 and 2 are for funds
+                        AND EXTRACT(YEAR FROM TransactionDate) = EXTRACT(YEAR FROM SYSDATE) 
+                        AND Username = :username
+                        GROUP BY TO_CHAR(TransactionDate, 'YYYY-MM')
+                        ORDER BY TO_CHAR(TransactionDate, 'YYYY-MM')
                         """, {'username': username})
-
     result_set = cur.fetchall()
-    data4 = []
-    labels4 = []
+    data2 = []
+    labels2 = []
     for i in result_set:
-        labels4.append(i[0])
-        data4.append(i[1])
-    request.session['data4'] = data4
-    request.session['labels4'] = labels4
-    context = {"request": request, "result": username, "data1": data, "labels1": labels, "data2": data2, "data3": data3,
-               "labels3": labels3, "data4": data4, "labels4": labels4}
-    return templates.TemplateResponse("analytics.html", context)
-
-
-@app.post("/analytics/{username}/pie_chart1", response_class=HTMLResponse)
-def pie_chart1(request: Request, username: str, selected_month: str = Form(...)):
-    username = request.session.get('username')
-    month = selected_month
-    result_set_cursor = cur.var(cx_Oracle.CURSOR)
-    cur.execute("BEGIN :result := get_expense_spending_per_month(:username,:month); END;",
-                result=result_set_cursor, username=username, month=month)
-    result_set = result_set_cursor.getvalue()
-    data = []
-    labels = []
-
-    for i in result_set:
-        labels.append(i[0])
-        data.append(i[1])
-    request.session['data1'] = data
-    request.session['labels1'] = labels
-    data2 = request.session.get('data2')
-    data3 = request.session.get('data3')
-    labels3 = request.session.get('labels3')
-    data4 = request.session.get('data4')
-    labels4 = request.session.get('labels4')
-    context = {"request": request, "data1": data, "labels1": labels, "result": username, "data2": data2, "data3": data3,
-               "labels3": labels3, "data4": data4, "labels4": labels4}
-    return templates.TemplateResponse("analytics.html", context)
-
-
-def piechart2(username, month):
+        labels2.append(datetime.strptime(i[0], '%Y-%m').strftime('%B'))
+        data2.append(i[1])
     cur.execute("""
-        SELECT INCOMETOTAL, EXPENSETOTAL, NETSAVINGS
-        FROM REPORTS
-        WHERE USERNAME = :username
-        AND EXTRACT(MONTH FROM ENDDATE) = :month
-    """, {'username': username, 'month': month})
-
+        SELECT SUM(amount) AS total_expenses
+        FROM Transactions
+        WHERE TransactionDate >= TRUNC(SYSDATE, 'MM') - INTERVAL '12' MONTH
+          AND TransactionDate < TRUNC(SYSDATE, 'MM')
+          AND CategoryId NOT IN (1, 2) AND Username = :username
+        """, {'username': username})
     result = cur.fetchone()
-
-    if result is not None:
-        tot_income = result[0] or 0
-        tot_expense = result[1] or 0
-        data = [tot_income, tot_expense]
-    else:
-        # Handle the case where no data is retrieved for the given username and month
-        data = [0, 0]
-
-    return data
-
-
-@app.post("/analytics/{username}/pie_chart2", response_class=HTMLResponse)
-def pie_chart2(request: Request, username: str, selected_month2: str = Form(...)):
-    username = request.session.get('username')
-    month = selected_month2
-    data = piechart2(username, month)
-    request.session['data2'] = data
-    data1 = request.session.get('data1')
-    labels1 = request.session.get('labels1')
-    data3 = request.session.get('data3')
-    labels3 = request.session.get('labels3')
-    data4 = request.session.get('data4')
-    labels4 = request.session.get('labels4')
-
-    context = {"request": request, "data2": data, "result": username, "data1": data1, "labels1": labels1,
-               "data3": data3, "labels3": labels3, "data4": data4, "labels4": labels4}
-    return templates.TemplateResponse("analytics.html", context)
+    total_expenses = result[0] or 0
+    context = {"request": request, "result": username, "data": data, "labels": labels, "data1": data1,
+               "labels1": labels1,
+               "data2": data2, "labels2": labels2, "total_expenses": total_expenses, "data_per": data_per}
+    return templates.TemplateResponse("statistics.html", context)
 
 
 if __name__ == "__main__":
     import uvicorn
-
-    uvicorn.run(app, host="127.0.0.1", port=4000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
